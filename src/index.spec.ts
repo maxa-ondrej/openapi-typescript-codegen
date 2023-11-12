a58061f1d2f8517ce3f1997cd847a0de9657f142
Ondřej Maxa <ondrej.maxa11@gmail.com>
2023-11-12 23:35:57 +0000

    ✅ expect writeClient to be ran
    Signed-off-by: Ondřej Maxa <ondrej.maxa11@gmail.com>

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,7 @@
 import OpenAPI from './index';
+import * as Writer from './utils/writeClient';
+
+const mock = jest.spyOn(Writer, 'writeClient');
 
 describe('index', () => {
   it('parses v2 without issues', async () => {
@@ -31,5 +34,33 @@ describe('index', () => {
       output: './generated/v3-downloaded/',
       write: false,
     });
+  });
+  it('parses and writes v2 without issues', async () => {
+    mock.mockClear();
+    mock.mockImplementation(() => Promise.resolve());
+    await OpenAPI.generate({
+      input: './test/spec/v2.json',
+      output: './generated/v2/',
+    });
+    expect(mock).toBeCalledTimes(1);
+  });
+
+  it('parses and writes v3 without issues', async () => {
+    mock.mockClear();
+    mock.mockImplementation(() => Promise.resolve());
+    await OpenAPI.generate({
+      input: './test/spec/v3.json',
+      output: './generated/v3/',
+    });
+    expect(mock).toBeCalledTimes(1);
+  });
+
+  it('fails with wrong input', async () => {
+    await expect(() =>
+      OpenAPI.generate({
+        input: { test: 1 },
+        output: './generated/v3/',
+      }),
+    ).rejects.toThrowError('Unsupported Open API version: "undefined"');
   });
 });
